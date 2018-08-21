7312debf618ecfecac7efb0d4be2dc4e14de35aa
AliDuzel <duzelali40@gmail.com>
2018-08-21 21:31:55 +0300

    Hakkında bölümü için webview eklenip www.magis.com.tr linkine yönlendirildi.

--- app/src/main/java/com/example/hp_pc/haberlistesi/HakkindaTab.java
+++ app/src/main/java/com/example/hp_pc/haberlistesi/HakkindaTab.java
@@ -2,16 +2,29 @@ package com.example.hp_pc.haberlistesi;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class HakkindaTab extends Fragment{
+public class HakkindaTab extends Fragment {
+    private WebView webView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.hakkindatab, container, false);
 
+        webView = (WebView) rootView.findViewById(R.id.aboutwebview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("http://www.magis.com.tr");
+        webView.setWebViewClient(new WebViewClient());
+
         return rootView;
     }
+
+
 }
